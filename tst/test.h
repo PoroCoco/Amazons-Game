9a49d3a6c61c9f077c803521fa32ea06572a73c9
KANIT BENSAIDI Abdurahman_El_Califa <Abdurahman_El_Califa.Kan_It_Bensaidi@bordeaux-inp.fr>
2023-03-24 14:38:14 +0100

    Adding tests files with dummy test

---
+++ tst/test.h
@@ -0,0 +1,16 @@
+
+#ifndef __TEST_PROJECT_H__
+#define __TEST_PROJECT_H__
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+
+/* ************************************************************************** */
+/*                                test dummy                                  */
+/* ************************************************************************** */
+
+int test_dummy(void);
+
+#endif
---
+++ tst/test_dummy.c
@@ -0,0 +1,6 @@
+#include"test.h"
+
+
+int test_dummy(void){
+    return 1;
+}
