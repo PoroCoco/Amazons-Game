91f2caf59c72eda5d844c3221ccb87a6794c637a
KANIT BENSAIDI Abdurahman_El_Califa <Abdurahman_El_Califa.Kan_It_Bensaidi@bordeaux-inp.fr>
2023-03-31 20:11:34 +0200

    Implementation of queen's functions

--- src/queens.c
+++ src/queens.c
@@ -1,15 +1,37 @@
 #include <stdlib.h>
 
 #include "queens.h"
+#include <assert.h>
 
 unsigned int queens_compute_number(unsigned int board_size){
-    return 0;
+    assert(board_size >= 5);
+    return 4*(board_size/10 + 1);
 }
 
-unsigned int* queens_create_positions(unsigned int queens_number, unsigned int player_id){
-    return NULL;
+unsigned int* queens_create_positions(unsigned int board_size, unsigned int player_id){
+    unsigned int queens_number = queens_compute_number(board_size);
+    unsigned int* queens_positions = malloc(sizeof(unsigned int) * queens_number);
+
+    unsigned int separing_gap = ((board_size - 2 * queens_number - 1) / queens_number) + 1; 
+
+    for(unsigned int index = 0; index < (queens_number / 4); index++){   
+        queens_positions[index] = separing_gap + index * separing_gap;
+        queens_positions[queens_number / 2 - index - 1] = board_size - 1 - queens_positions[index]; //same for the symetric
+    }
+
+    for(unsigned int index = queens_number / 2; index < 3 * (queens_number / 4); index = index + 2){        
+        queens_positions[index] = (separing_gap + index * separing_gap) * board_size ;
+        queens_positions[index + 1] = queens_positions[index] + board_size - 1 + 10;
+    }
+
 }
 
-unsigned int* queens_copy(const unsigned int *queens, unsigned int queens_number){
+void queens_free_positions(unsigned int *queens){
+    assert(queens);
+    free(queens);
+}
+
+unsigned int* queens_copy(const unsigned int *queens, unsigned int board_size){
+    
     return NULL;
 }
--- src/queens.h
+++ src/queens.h
@@ -2,20 +2,32 @@
 #define _AMAZON_QUEENS_H
 
 /// @brief Compute the number of queens for the given board size
+/// @param board_size
 /// @return The number of queens that each player should have
+/// @pre @p board_size must be greater that 5 or equal
 unsigned int queens_compute_number(unsigned int board_size);
 
 /// @brief Creates an array with the position of all the queens at the beginning of the game for the given player.
 /// @param queens_number The number of queens for this player
 /// @param player_id The player
 /// @return A heap allocated array of size queens_number containing the position of each queen for the player.
-unsigned int* queens_create_positions(unsigned int queens_number, unsigned int player_id);
+unsigned int* queens_create_positions(unsigned int board_size, unsigned int player_id);
+
+/// @brief Frees the allocated memory to the array of positions
+/// @param queens The array to free
+/// @pre @p queens must be a valid pointer toward a an array.
+void queens_free_positions(unsigned int *queens);
+
 
 /// @brief Creates a copy of the array of queens
 /// @param queens The array to copy
 /// @param queens_number The size of queens
 /// @return A heap allocated array with the same values as queens
-unsigned int* queens_copy(const unsigned int *queens, unsigned int queens_number);
+unsigned int* queens_copy(const unsigned int *queens, unsigned int board_size);
+
+
+void queens_move(unsigned int *queens, unsigned int src, unsigned int dst);
+
 
 
 #endif // _AMAZON_QUEENS_H
