acd852f17a3628d6f629dec99260dbfe37e35916
KANIT BENSAIDI Abdurahman_El_Califa <Abdurahman_El_Califa.Kan_It_Bensaidi@bordeaux-inp.fr>
2023-04-06 23:24:40 +0200

    QUEENS : Memory leak tests correction  and changing desc of header

--- src/queens.h
+++ src/queens.h
@@ -4,20 +4,18 @@
 #include <stdbool.h>
 
 /// @brief Compute the number of queens for the given board size
-/// @param board_size
+/// @param board_width
 /// @return The number of queens that each player should have
-/// @pre @p board_size must be greater that 5 or equal
-unsigned int queens_compute_number(unsigned int board_size);
+unsigned int queens_compute_number(unsigned int board_width);
 
 /// @brief Creates an array with the position of all the queens at the beginning of the game for the given player.
 /// @param queens_number The number of queens for this player
 /// @param player_id The player
 /// @return A heap allocated array of size queens_number containing the position of each queen for the player.
-unsigned int* queens_create_positions(unsigned int board_size, unsigned int player_id);
+unsigned int* queens_create_positions(unsigned int board_width, unsigned int player_id);
 
 /// @brief Frees the allocated memory to the array of positions
 /// @param queens The array to free
-/// @pre @p queens must be a valid pointer toward a an array.
 void queens_free_positions(unsigned int *queens);
 
 
@@ -27,12 +25,17 @@ void queens_free_positions(unsigned int *queens);
 /// @return A heap allocated array with the same values as queens
 unsigned int* queens_copy(const unsigned int *queens, unsigned int queen_count);
 
-
+/// @brief Changes the position of a queen
+/// @param queens The array to modify
+/// @param src The queen to move
+/// @param dst The destination
 void queens_move(unsigned int *queens, unsigned int src, unsigned int dst);
 
-
-
-
+/// @brief Tests if an position occupied by a queen 
+/// @param queens The checked array
+/// @param index The position to check
+/// @param board_width Size of the boards the @p queens are placed in
+/// @return true if queens contains index
 bool queens_occupy(const unsigned int *queens, unsigned int index, unsigned int board_width);
 
 
--- tst/test_queens.c
+++ tst/test_queens.c
@@ -50,29 +50,32 @@ static void print_game(int* array, int size, int m){
 }
 int test_queens_create_positions(void){
     int from = 5;
-    int to = 5;
+    int to = 15;
     unsigned int* positions_to_print;
     for(int i = from; i < to; i++){ // prints the games of size i twice with the queens of each player 
         positions_to_print = queens_create_positions(i,0);
         print_game(positions_to_print,queens_compute_number(i),i);
+        queens_free_positions(positions_to_print);
         positions_to_print = queens_create_positions(i,1);
         print_game(positions_to_print,queens_compute_number(i),i);
+        queens_free_positions(positions_to_print);
     }
     unsigned int* computed_positions_5 = queens_create_positions(5,0);
     int positions_5[4] = {1,3,5,9};
-    if(!contain_the_same_elements(computed_positions_5,positions_5,4))
-        return 1;
+    int result_5 = contain_the_same_elements(computed_positions_5,positions_5,4);
+    queens_free_positions(computed_positions_5);
+
     unsigned int* computed_positions_12 = queens_create_positions(12,1);
     int positions_12[8] = {142,140,133,135,131,120,96,107};
-    if(!contain_the_same_elements(computed_positions_12,positions_12,8))
-        return 1;
+    int result_12 = contain_the_same_elements(computed_positions_12,positions_12,8);
+    queens_free_positions(computed_positions_12);
 
     unsigned int* computed_positions_15 = queens_create_positions(15,0);
     int positions_15[8] = {2,5,9,12,30,75,44,89};
-    if(!contain_the_same_elements(computed_positions_15,positions_15,8))
-        return 1;
+    int result_15 = contain_the_same_elements(computed_positions_15,positions_15,8);
+    queens_free_positions(computed_positions_15);
 
-    return 0;
+    return !(result_12 && result_15 && result_5);
 }
 
 int test_queens_copy(void){
