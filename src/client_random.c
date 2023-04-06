3484b51e3632063a81ebf884264bbdfa60df5d44
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2023-04-06 20:45:42 +0200

    Adapted move valid to use diagonals

--- src/client_random.c
+++ src/client_random.c
@@ -7,39 +7,46 @@ struct random_client *c = NULL;
 
 enum dir_t get_move_direction(random_client_t *client, unsigned int origin, unsigned int destination)
 {
-    unsigned int width = board_width(client->board);
     if (origin == destination) return DIR_ERROR;
+    unsigned int width = board_width(client->board);
+    unsigned int origin_x = origin % width;
+    unsigned int origin_y = origin / width;
 
-    int difference = origin - destination;
-    if (abs(difference) < width){
-        return origin - destination > 0 ? DIR_EAST : DIR_WEST;
+    unsigned int destination_x = destination % width;
+    unsigned int destination_y = destination / width;
+
+    //origin and destination on the same column
+    if (origin_x == destination_x){
+        return origin_y > destination_y ? DIR_NORTH : DIR_SOUTH;
     }
 
-    if ((destination % width) == (origin % width)){
-        return origin - destination > 0 ? DIR_NORTH : DIR_SOUTH;
+    //origin and destination on the same line
+    if (origin_y == destination_y){
+        return origin_x > destination_x ? DIR_WEST : DIR_EAST;
+    }
+
+    if(destination_x > origin_x){
+        return destination_y > origin_y ? DIR_SE : DIR_NE;
+    }
+
+    if(destination_x < origin_x){
+        return destination_y > origin_y ? DIR_SW : DIR_NW;
     }
 
     return DIR_ERROR;
 }
 
-bool is_move_valid(struct random_client *c, struct move_t *move)
-{
-    int width = board_width(c->board);
-    enum dir_t direction = get_move_direction(c, move->queen_src, move->queen_dst);
-    if (direction == DIR_ERROR) return false;
-
-    unsigned int current_position = move->queen_src;
-    unsigned int destination = move->queen_dst;
-
+int compute_step_toward_direction(enum dir_t direction, unsigned int board_width){
     int step = 0;
+
     switch (direction)
     {
     case DIR_NORTH:
-        step = -width;
+        step = -board_width;
         break;
     
     case DIR_SOUTH:
-        step = width;
+        step = board_width;
         break;
 
     case DIR_EAST:
@@ -50,24 +57,54 @@ bool is_move_valid(struct random_client *c, struct move_t *move)
         step = -1;
         break;
 
+    case DIR_NW:
+        step = -1 - board_width;
+        break;
+
+    case DIR_NE:
+        step = 1 - board_width;
+        break;
+
+    case DIR_SW:
+        step = -1 + board_width;
+        break;
+
+    case DIR_SE:
+        step = 1 + board_width;
+        break;
+
     default:
         fprintf(stderr, "Impossible direction given to go from move source to move destination\n");
         exit(EXIT_FAILURE);
-        break;
     }
+    
+    assert(step != 0);
+    return step;
+}
+
+bool is_move_valid(struct random_client *c, struct move_t *move)
+{
+    unsigned int current_position = move->queen_src;
+    unsigned int destination = move->queen_dst;
+
+    //check if there is a queen on the destination or source position
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        if (queens_occupy(c->board->queens[i], destination, c->board->board_width)) return false;
+        if (queens_occupy(c->board->queens[i], current_position, c->board->board_width)) return false;
+    }
+    
+
+    int width = board_width(c->board);
+    enum dir_t direction = get_move_direction(c, move->queen_src, move->queen_dst);
+    if (direction == DIR_ERROR) return false;
+    
+    int step = compute_step_toward_direction(direction, width);
 
     while(current_position != destination){
         current_position += step;
         if(!board_index_is_available(c->board, current_position)) return false;
     }
-
-    // check is there is a queen on the destination edge
-    if (queens_occupy(c->board->queens[0], move->queen_dst, c->board->board_width)) return false;
-    if (queens_occupy(c->board->queens[1], move->queen_dst, c->board->board_width)) return false;
-
-    // check is there is a queen on the source edge
-    if (queens_occupy(c->board->queens[0], move->queen_src, c->board->board_width)) return false;
-    if (queens_occupy(c->board->queens[1], move->queen_src, c->board->board_width)) return false;
 
     return true;
 }
