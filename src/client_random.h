aaf048bb0ba47a218488986d727ae2dd11d50a66
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2023-04-07 15:55:21 +0200

    removed old prototype

--- src/client_random.h
+++ src/client_random.h
@@ -16,8 +16,5 @@ typedef struct random_client{
     board_t *board;
 } random_client_t;
 
-enum dir_t get_move_direction(random_client_t *client, unsigned int origin, unsigned int destination);
-
-bool is_move_valid(random_client_t *c, struct move_t *move);
 
 #endif
