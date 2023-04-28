2d4d25ed21d3fc4eddaeb4f57fa15d46de9ad1a8
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2023-04-28 17:56:08 +0200

    script compute all coeffs

--- compute_best_coeff.sh
+++ compute_best_coeff.sh
@@ -3,7 +3,14 @@
 
 for i in $(seq 0.0 0.1 1.0)
 do
-    echo "gcc -D$i"
+    make client_new_heuristic.so -e COEFF=$i
+    echo ""
+    echo "########################################" >> coeff_results.txt
+    echo "COEFF = $i" >> coeff_results.txt
+    ./install/arena -r 30 ./install/client_power_heuristic.so ./install/client_new_heuristic.so >> coeff_results.txt
+    echo ""
+    echo ""
+    ./install/arena -r 30 ./install/client_new_heuristic.so ./install/client_power_heuristic.so >> coeff_results.txt
 done
 
 
