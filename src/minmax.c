36364437e47f35d2cf3b779bf74ffdc901310034
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2023-05-03 11:27:35 +0200

    header

--- src/minmax.c
+++ src/minmax.c
@@ -1,14 +1,25 @@
 #include <assert.h>
 
-
-
 //node header
 
+#include "board.h"
+#include "move.h"
+
+typedef struct node{
+    struct node **childs;
+    struct node *parent;
+    size_t child_count;
+    size_t allocated;
+    struct move_t move;
+} node_t;
 
 
-
-
-
+bool isLeaf(node_t *node);
+void add_child(node_t *node, node_t *child);
+void remove_child(node_t *node, node_t child);
+node_t *create_empty_node(node_t *parent);
+void destroy_tree(node_t *root);
+void display_node(node_t *node);
 
 //header
 
@@ -65,3 +76,6 @@ void display_node(node_t *node){
     _display_move(node->move);
     printf("\n");
 }
+
+
+
