ebb80202d61b636f37cb378058bf221672f6351b
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2023-05-08 18:42:46 +0200

    fixed tree tests

--- tst/test_tree.c
+++ tst/test_tree.c
@@ -13,9 +13,12 @@ int test_node_create(void) {
     queens[1] = queens_create_positions(board_width, 1);
     board_t *b = board_create(create_graph(board_width, SQUARE), queens, queens_compute_number(board_width));
     
-    struct move_t m = {.queen_src = 2, .queen_dst = 20, .arrow_dst = 18};
+    struct move_t *m = malloc(sizeof(*m));
+    m->queen_src = 2;
+    m->queen_dst = 20;
+    m->arrow_dst = 18;
 
-    struct node* node = node_create(0, 0, b, &m);
+    struct node* node = node_create(0, 0, b, m);
 
     node_free(node);
     board_free(b);
@@ -29,11 +32,18 @@ int test_node_add(void) {
     queens[1] = queens_create_positions(board_width, 1);
     board_t *b = board_create(create_graph(board_width, SQUARE), queens, queens_compute_number(board_width));
     
-    struct move_t first_move = {.queen_src = 2, .queen_dst = 20, .arrow_dst = 18};
-    struct move_t second_move = {.queen_src = 1, .queen_dst = 17, .arrow_dst = 16};
+    struct move_t *first_move = malloc(sizeof(*first_move));
+    first_move->queen_src = 2;
+    first_move->queen_dst = 20;
+    first_move->arrow_dst = 18;
 
-    struct node* root = node_create(0, 0, b, &first_move);
-    struct node* child = node_create(1, 2, b, &second_move);
+    struct move_t *second_move = malloc(sizeof(*second_move));
+    second_move->queen_src = 1;
+    second_move->queen_dst = 17;
+    second_move->arrow_dst = 16;
+
+    struct node* root = node_create(0, 0, b, first_move);
+    struct node* child = node_create(1, 2, b, second_move);
 
     assert(node_add(root, child) == 1);
 
